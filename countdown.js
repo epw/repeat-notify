08e7eb6118514754e5e14dbb1d37919598328cfe
Eric Willisson <ericwillisson@gmail.com>
2011-11-24 17:44:24 -0500

    Forgot countdown javascript

---
+++ countdown.js
@@ -0,0 +1,28 @@
+var seconds;
+
+function seconds_to_time (num_seconds) {
+    var minutes = num_seconds / 60 + "";
+    if (minutes < 10) {
+	minutes = "0" + minutes;
+    }
+    var these_seconds = num_seconds % 60 + "";
+    if (these_seconds < 10) {
+	these_seconds = "0" + these_secods;
+    }
+    return minutes + ":" + these_seconds;
+}
+
+function countdown () {
+    document.getElementById ("countdown").innerHTML = seconds_to_time (seconds);
+}    
+
+function init () {
+    var minutes = window.location.getParameter ("t");
+    seconds = minutes * 60;
+
+    setInterval ("document.getElementById ('notify').play ()",
+		 1000 * seconds);
+    addInterval ("countdown", 1);
+}
+
+window.addEventHandler ("load", init);
