0ee57f2c4ae4d3ece86be882d4199be1ae05c9e0
Eric Willisson <ericwillisson@gmail.com>
2011-11-24 18:08:46 -0500

    Made window wider

--- startnotify.js
+++ startnotify.js
@@ -2,7 +2,7 @@ function init () {
     window.open (window.location.origin + window.location.pathname
 		 + "countdown.html?t=" + window.location.getParameter ("t"),
 		 "countdown",
-		 "width=100, height=100");
+		 "width=150, height=100");
 }
 
 window.addEventListener ("load", init);
