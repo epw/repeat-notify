0aec1e95d879c8a266b6d64eb8134a463b39463b
Eric Willisson <ericwillisson@gmail.com>
2011-11-24 17:57:54 -0500

    resizing popup to be small correctly

--- startnotify.js
+++ startnotify.js
@@ -1,7 +1,7 @@
 function init () {
     window.open (window.location.origin + window.location.pathname
 		 + "countdown.html?t=" + window.location.getParameter ("t"),
-		 specs="width=100, height=100");
+		 "width=100, height=100");
 }
 
 window.addEventListener ("load", init);
