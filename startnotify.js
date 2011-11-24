5e36c524686f77fd7099d1e8f7f4eef82c28b67d
Eric Willisson <ericwillisson@gmail.com>
2011-11-24 18:16:34 -0500

    Start window no longer starts countdown with no option

--- startnotify.js
+++ startnotify.js
@@ -1,8 +1,10 @@
 function init () {
-    window.open (window.location.origin + window.location.pathname
-		 + "countdown.html?t=" + window.location.getParameter ("t"),
-		 "countdown",
-		 "width=150, height=100");
+    if (window.location.getParameter ("t") != "") {
+	window.open (window.location.origin + window.location.pathname
+		     + "countdown.html?t=" + window.location.getParameter ("t"),
+		     "countdown",
+		     "width=150, height=100");
+    }
 }
 
 window.addEventListener ("load", init);
