2beba3a38c9b0f07162bd5d806232d4a973c9b4c
Eric Willisson <ericwillisson@gmail.com>
2011-11-24 18:56:33 -0500

    Putting in sound query string to be passed along

--- startnotify.js
+++ startnotify.js
@@ -1,7 +1,7 @@
 function init () {
     if (window.location.getParameter ("t") != "") {
 	window.open (window.location.origin + window.location.pathname
-		     + "countdown.html?t=" + window.location.getParameter ("t"),
+		     + "countdown.html" + window.location.search,
 		     "countdown",
 		     "width=150, height=100");
     }
