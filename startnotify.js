6a6fc4505b3716ad39725d20c776d26854b091f9
Eric Willisson <ericwillisson@gmail.com>
2011-11-24 19:11:41 -0500

    Enabled javascript

--- startnotify.js
+++ startnotify.js
@@ -1,3 +1,7 @@
+function sndchange (val) {
+    document.getELementById ("snd").value = val;
+}
+
 function init () {
     if (window.location.getParameter ("t") != "") {
 	window.open (window.location.origin + window.location.pathname
@@ -5,6 +9,8 @@ function init () {
 		     "countdown",
 		     "width=150, height=100");
     }
+
+    document.getElementById ("sndlist").addEventListener ("change", sndchange);
 }
 
 window.addEventListener ("load", init);
